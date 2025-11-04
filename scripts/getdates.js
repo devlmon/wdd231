759567974c89694b8d1219cf2132b2f6eec5ae5b
devlmon <lmontenegrollamozas@byupathway.edu>
2025-11-04 12:46:45 +0100

    W01 Assignment: Course Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const today = new Date();
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+lastModified.innerHTML = `Last modification: ${document.lastModified}`;
---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+// Navigation Script
+const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
